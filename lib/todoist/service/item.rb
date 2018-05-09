65743f2d201f8553d0ffa75bde08e8e35c6bed8c
Stefan Siegl <stesie@brokenpipe.de>
2018-05-09 23:52:43 +0200

    Use new queue api

--- lib/todoist/service/item.rb
+++ lib/todoist/service/item.rb
@@ -2,18 +2,18 @@ module Todoist
   module Service
     class Item < BaseService
       def update(id, arguments)
-        @client.add_to_queue(create_command('item_update', arguments.merge({id: id})))
+        @client.queue.add(create_command('item_update', arguments.merge({id: id})))
       end
 
       def delete(ids)
         if ids.is_a?(FixNum)
           ids = [ids]
         end
-        @client.add_to_queue(create_command('item_delete', {ids: ids}))
+	@client.queue.add(create_command('item_delete', {ids: ids}))
       end
 
       def close(id)
-        @client.add_to_queue(create_command('item_close', {id: id}))
+        @client.queue.add(create_command('item_close', {id: id}))
       end
 
       def collection_class
